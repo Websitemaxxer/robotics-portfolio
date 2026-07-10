41694d2228e62027b026c6ff38e3287e9feb136a
Kush Kumar <k.kumar@ankvt.com>
2026-07-10 20:33:11 +0400

    Update portfolio content

--- projects/03_arduino-coding-challenges/04_code/challenge_01_reaction_timer/reaction_timer.ino
+++ projects/03_arduino-coding-challenges/04_code/challenge_01_reaction_timer/reaction_timer.ino
@@ -1,17 +1,3 @@
-/*
-  Reaction Timer  (post-kit coding challenge #1)
-
-  A reflex game: after a random 2-5 second wait the LED turns on (GO!),
-  and you press the button as fast as you can. The Serial Monitor prints
-  your reaction time in milliseconds. Pressing before the LED comes on is
-  a false start ("Too soon!") and restarts the round.
-
-  Pins:
-    LED (+ 220 ohm to GND) ... D8
-    Pushbutton ............... D2  (10k pull-down to GND, other side to +5V)
-    Serial ................... 9600 baud
-*/
-
 int ledPin = 8;
 int buttonPin = 2;
 
@@ -24,8 +10,8 @@ void setup() {
 void loop() {
   digitalWrite(ledPin, LOW);
 
-  long waitUntil = millis() + random(2000, 5001);   // pick the GO moment once
-  while (millis() < waitUntil) {                     // wait, watching for a cheat
+  long waitUntil = millis() + random(2000, 5001);
+  while (millis() < waitUntil) {
     if (digitalRead(buttonPin) == HIGH) {
       Serial.println("Too soon!");
       delay(1000);
@@ -35,7 +21,7 @@ void loop() {
 
   digitalWrite(ledPin, HIGH);
   long start = millis();
-  while (digitalRead(buttonPin) == LOW) { }          // wait for the real press
+  while (digitalRead(buttonPin) == LOW) { }
   Serial.print("Reaction time: ");
   Serial.print(millis() - start);
   Serial.println(" ms");
